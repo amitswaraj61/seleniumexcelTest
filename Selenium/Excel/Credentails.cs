215c029f619864d939b0e0de37fbea3f7ac28edc
aswaraj78 <aswaraj78@hotmail.com>
2020-07-11 12:27:56 +0530

    Add project files.

---
+++ Selenium/Excel/Credentails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Selenium.Excel
+{
+   public  class Credentails
+    {
+        public  string userName = "";
+        public  string pass = "";
+        public Credentails()
+        {
+           
+           
+                ExcelOperations.PopulateInCollection(@"C:\Users\Amit\Desktop\Amit.xlsx");
+               userName = ExcelOperations.ReadData(1, "userName");
+                Console.WriteLine("userName"+userName);
+               pass = ExcelOperations.ReadData(1, "password");
+                Console.WriteLine("password"+pass);
+
+                
+            
+           
+
+        }//
+
+        
+
+
+
+    }
+}
---
+++ Selenium/Excel/LoginTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Selenium.Excel
+{
+    [TestFixture]
+    class LoginTest
+    {
+        [Test]
+        public void loginTest()
+        {
+            Credentails credentials = new Credentails();
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            //Maximizing the window
+            driver.Manage().Window.Maximize();
+
+            //Enter the url
+            driver.Url = "https://www.linkedin.com/login";
+
+            IWebElement username = driver.FindElement(By.XPath("//input[contains(@id,'username')]"));
+
+
+            username.SendKeys(credentials.userName);
+            Thread.Sleep(3000);
+
+            
+            string pa = ExcelOperations.ReadData(1, "password");
+            IWebElement pas = driver.FindElement(By.XPath("//input[contains(@id,'password')]"));
+            
+          
+            pas.SendKeys(credentials.pass);
+        
+            Thread.Sleep(3000);
+            driver.Quit();
+        }
+    }
+}
